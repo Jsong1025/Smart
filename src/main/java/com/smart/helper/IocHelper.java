ba27f4886d98a8b53c019e847f3695cbef0bff5f
Jsong <songfengjv@sina.cn>
2015-11-15 10:57:48 +0800

    实现依赖注入

---
+++ src/main/java/com/smart/helper/IocHelper.java
@@ -0,0 +1,48 @@
+package com.smart.helper;
+
+import com.smart.annotation.Inject;
+import com.smart.util.ArrayUtil;
+import com.smart.util.CollectionUtil;
+import com.smart.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * 依赖注入助手类
+ *
+ * @author jsong
+ */
+public final class IocHelper {
+
+    static {
+        // 获取所有的Bean类与Bean实例之间的映射关系（Bean Map）
+        Map<Class<?> , Object> beanMap = BeanHelper.getBeanMap();
+        if (CollectionUtil.isNotEmpty(beanMap)) {
+            // 遍历Bean Map
+            for (Map.Entry<Class<?> , Object> beanEntity : beanMap.entrySet()) {
+                // 从BeanMap中获取Bean类与Bean实例
+                Class<?> beanClass = beanEntity.getKey();
+                Object beanInstance = beanEntity.getValue();
+                // 获取Bean类定义的所有成员变量（Bean Field）
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if (ArrayUtil.isNotEmpty(beanFields)) {
+                    // 遍历Bean Field
+                    for (Field beanField : beanFields){
+                        // 判断当前Bean Field是否带有Inject注解
+                        if (beanField.isAnnotationPresent(Inject.class)) {
+                            // 在Bean Map中获取Bean Field对应的实例
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null) {
+                                // 通过反射初始化Bean Field的值
+                                ReflectionUtil.setField(beanInstance , beanField ,beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/smart/util/ArrayUtil.java
@@ -0,0 +1,26 @@
+package com.smart.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * 数组工具类
+ *
+ * @author jsong
+ */
+public final class ArrayUtil {
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isNotEmpty (Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty (Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+
+}
